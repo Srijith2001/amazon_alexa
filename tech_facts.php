a1e897fcf2223e2055111f67e13e412476316aea
Srijith Bharadwaj <srijithdamerla2001@gmail.com>
2019-03-10 04:48:01 +0000

    Add tech_facts.php

---
+++ tech_facts.php
@@ -0,0 +1,30 @@
+[  
+   {  
+      "Number":"1",
+      "Fact":"The very first Apple logo featured Sir Isaac Newton sitting underneath a tree, with an apple about to hit his head."
+   },
+   {  
+      "Number":"2",
+      "Fact":"Google rents out goats from a company called California Grazing to help cut down the amount of weeds and brush at Google HQ!"
+   },
+   {  
+      "Number":"3",
+      "Fact":"You cannot reverse a Bitcoin transaction, or be forced to pay."
+   },
+   {  
+      "Number":"4",
+      "Fact":"The average computer user blinks seven times a minute, the normal rate is twenty times per minute."
+   },
+   {  
+      "Number":"5",
+      "Fact":"On an average work day, a typist's fingers travel about 12.6 miles."
+   },
+   {  
+      "Number":"6",
+      "Fact":"The radio took 38 years to reach a market audience of 50 million. The television took 13 years and the iPod only took 3 years to reach a market audience of 50 million."
+   },
+   {  
+      "Number":"7",
+      "Fact":"Using a hands-free device to talk on the phone while driving is shown to be equally or more dangerous than driving drunk."
+   }
+]
